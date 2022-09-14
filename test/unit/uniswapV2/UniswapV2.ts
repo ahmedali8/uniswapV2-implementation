acb74c253d872bb49314ce2b6116f15326838666
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-09-14 10:23:21 +0500

    build: remove stale deps

--- test/unit/uniswapV2/UniswapV2.ts
+++ test/unit/uniswapV2/UniswapV2.ts
@@ -2,7 +2,7 @@ import { shouldBehaveLikeUniswapV2 } from "./UniswapV2.behavior";
 import { uniswapV2Fixture } from "./UniswapV2.fixture";
 
 export function testUniswapV2(): void {
-  describe("Uniswap", function () {
+  describe("UniswapV2", function () {
     beforeEach(async function () {
       const { router, factory, weth9, token0, token1 } = await this.loadFixture(uniswapV2Fixture);
       this.contracts.uniswapV2 = { router, factory, weth9, token0, token1 };
